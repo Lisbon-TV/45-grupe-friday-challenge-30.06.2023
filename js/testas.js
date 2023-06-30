aa795866bd32828091307cc7e6ff62d1056683d8
Lisbon <lisbon@dublin.com>
2023-06-30 12:54:37 +0300

    Exc. 1 & 2

--- js/testas.js
+++ js/testas.js
@@ -1,41 +1,68 @@
 console.clear();
-console.log('veika');
 
 // 1. Palyginti du skaičius a ir b. Išvesti į konsolę, kuris didesnis arba jie lygūs. (4 taškai)
+console.log('\n');
+
+const a = 5;
+
+
+function greaterOrEqual(val) {
+    if (val > a) {
+    return `Tavo skaičius yra didesnis.`;
+    }
+    if (val === a) {
+    return `Mūsų skaičiai yra lygūs!`;
+    }
+    return `Error: Skaičiai netenkina duotos sąlygos!`;
+}
+
+console.log(greaterOrEqual(12));
+console.log(greaterOrEqual(5));
+
+console.log(greaterOrEqual(1));
+console.log(greaterOrEqual(-100));
+console.log(greaterOrEqual(0));
+
+
 
 
 // 2. Naudojant for ciklą, išvesti į konsolę skaičius nuo 1 iki 10. (5 taškai)
+console.log('\n');
+
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+}
 
 
 // 3. Naudojant for ciklą, išvesti į konsolę skaičius nuo 0, 2, 4, 6, 8, 10. (5 taškai)
-
+console.log('\n');
 
 // 4. Naudojant for ciklą, sugeneruoti penkis atsitiktinius skaičius nuo 1 iki 10. Išvesti juos konsolėje. (5 taškai)
-
+console.log('\n');
 
 // 5. Naudojant while ciklą, spausdinti atsitiktinius skaičius nuo 1 iki 10. Paskutinis atspausdintas skaičius turi būti 5. (7 taškai)
-
+console.log('\n');
 
 // 6. Sukurti masyvą, kurio ilgis būtų nuo 20 iki 30, o reikšmės būtų skaičiai nuo 10 iki 30. Surasti didžiausią masyvo reikšmę, NENAUDOJANT sort() bei Math.max() funkcijų. (7 taškai)
-
+console.log('\n');
 
 // 7. Sugeneruokite masyvą, kurio reikšmės atsitiktinės raidės A, B, C ir D, o ilgis 100. Suskaičiuokite kiek yra kiekvienos raidės. (7 taškai)
-
+console.log('\n');
 
 // 8. Parašyti funkciją - lygineSuma. Funkcijos parametrai - du kintamieji. Testų reikalavimai - abu kitamieji turi būti arba skaičiai arba masyvai(negali būti vienas skaičius, kitas masyvas).
-
+console.log('\n');
 
 // Jei kintamieji skaičiai, grąžinti skaičių sumą, jei kintamieji masyvai - grąžinti masyvų ilgių sumą. Jei abu kintamieji skaičiai arba masyvai, bet suma nelyginė - grąžinti tekstą, kad suma nelyginė. (10 taškų)
 
 
 // 9. Parašyti funkciją pirminisSkaicius. Funkcija turi vieną kintamąjį. Turi būti patikrinimas, kad kintamasis yra skaičius. Funkcija turi grąžinti ar pateiktas skaičius yra pirminis( pirminis
-
+console.log('\n');
 
 // skaičius yra tas, kuris dalinasi tik iš savęs ir tik iš vieneto be liekanos.) (10 taškų)
 
 
 // 10. Parašyti funkciją telefonoNumeris. Funkcija turi priimti vieną kintamąjį - masyvą. Masyvo elementai - skaičiai, ilgis - 10. Funkcija turi grąžinti telefono numerį tokiu formatu - 
-
+console.log('\n');
 
 // "(XXX) XXX-XXXX". (10 taškų)
 
