d19102d23d80fa86b532cadbc9ce5353c0277bd0
Lisbon <lisbon@dublin.com>
2023-06-30 13:44:43 +0300

    Exc. 4

--- js/testas.js
+++ js/testas.js
@@ -46,6 +46,13 @@ for (let i = 0; i <= 10; i++) {
 // 4. Naudojant for ciklą, sugeneruoti penkis atsitiktinius skaičius nuo 1 iki 10. Išvesti juos konsolėje. (5 taškai)
 console.log('\n');
 
+
+for (let i = 0; i < 5; i++) {
+    const randomNo = Math.floor(Math.random() * 10) + 1;
+    console.log(randomNo);
+}
+
+
 // 5. Naudojant while ciklą, spausdinti atsitiktinius skaičius nuo 1 iki 10. Paskutinis atspausdintas skaičius turi būti 5. (7 taškai)
 console.log('\n');
 
